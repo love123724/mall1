fac2f21616ee2dff88a5f3944bc5d240194a9c43
love123724 <53546512+love123724@users.noreply.github.com>
2020-05-12 13:52:56 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
    const instance=axios.create({
-       baseURL:"http://123.207.32.32:8000/api/m3",
+       baseURL:"http://123.207.32.32:8000",
        timeout:8000
    })
     return instance(config)
